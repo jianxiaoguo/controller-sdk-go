8615a7a4edbe62b15db42778cdd8f5d7b1028bc9
duanhongyi <duanhongyi@doopai.com>
2024-07-17 16:56:56 +0800

    chore(filer): listdir add path field

--- api/filer.go
+++ api/filer.go
@@ -2,6 +2,7 @@ package api
 
 type FilerDirEntry struct {
 	Name      string `json:"name,omitempty"`
+	Path      string `json:"path,omitempty"`
 	Size      string `json:"size,omitempty"`
 	Type      string `json:"type,omitempty"`
 	Timestamp string `json:"timestamp,omitempty"`
